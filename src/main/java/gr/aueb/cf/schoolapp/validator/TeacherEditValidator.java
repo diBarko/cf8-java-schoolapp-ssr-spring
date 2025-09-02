350b4a8688f02eb4ee1c360971bdf45eee60c826
Denis Barko <denisbarko@hotmail.com>
2025-09-02 22:52:10 +0300

    removed useless comments

--- src/main/java/gr/aueb/cf/schoolapp/validator/TeacherEditValidator.java
+++ src/main/java/gr/aueb/cf/schoolapp/validator/TeacherEditValidator.java
@@ -1,7 +1,5 @@
 package gr.aueb.cf.schoolapp.validator;
 
-import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
-import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
 import gr.aueb.cf.schoolapp.dto.TeacherEditDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import gr.aueb.cf.schoolapp.repository.RegionRepository;
@@ -38,16 +36,5 @@ public class TeacherEditValidator implements Validator {
                 errors.rejectValue("vat", "vat.teacher.exists");
             }
         }
-
-//        if (teacherEditDTO.getVat() != null && teacherRepository.findByVat(teacherEditDTO.getVat()).isPresent()) {
-//            log.error("Save failed for teacher with vat={}. Teacher already exists", teacherEditDTO.getVat());
-//            errors.rejectValue("vat", null, "Το ΑΦΜ του Καθηγητή υπάρχει ήδη.");
-//        }
-
-//        if (teacherEditDTO.getRegionId() != null && regionRepository.findById(teacherEditDTO.getRegionId()).isEmpty()) {
-//            log.error("Save failed for teacher with vat={}. Region not found with id={}",
-//                    teacherEditDTO.getVat(), teacherEditDTO.getRegionId());
-//            errors.rejectValue("regionId", null, "Η περιοχή του Καθηγητή δεν μπορεί να είναι κενή.");
-//        }
     }
 }
