4e14ac15d4fabe53d510abf25c982d0b181cede0
Denis Barko <denisbarko@hotmail.com>
2025-09-07 19:26:23 +0300

    removed message parameters, as they're defined in messages_xx.properties

--- src/main/java/gr/aueb/cf/schoolapp/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/UserInsertDTO.java
@@ -1,5 +1,6 @@
 package gr.aueb.cf.schoolapp.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,11 +14,14 @@ import lombok.Setter;
 @Setter
 public class UserInsertDTO {
 
+    @NotNull
     @Size(min = 2, max = 20)
     private String username;
 
+    @NotNull
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
     private String password;
 
+    @NotNull
     private Long roleId;
 }
