1b322ddeed79fc5bc08a69bd80fa239e9f31ab9e
Denis Barko <denisbarko@hotmail.com>
2025-09-07 19:20:29 +0300

    removed message parameters, as they're defined in messages_xx.properties

--- src/main/java/gr/aueb/cf/schoolapp/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/UserInsertDTO.java
@@ -14,17 +14,11 @@ import lombok.Setter;
 @Setter
 public class UserInsertDTO {
 
-    @NotNull(message = "Το username δεν μπορεί να είναι κενό.")
-    @Size(min = 2, max = 20, message = "Το username πρέπει να είναι μεταξύ 2 - 20 χαρακτήρες.")
+    @Size(min = 2, max = 20)
     private String username;
 
-    @NotNull(message = "Το password δεν μπορεί να είναι null.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$",
-            message = "Το password πρέπει να περιέχει τουλάχιστον 1 πεζό, 1 κεφαλαίο, 1 ψηφίο και 1 ειδικό χαρακτήρα χωρίς κενά.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
     private String password;
-
-//    @NotNull(message = "Ο ρόλος δεν μπορεί να είναι κενός")
-//    private String role;
 
     @NotNull(message = "Ο ρόλος δεν μπορεί να είναι κενός")
     private Long roleId;
