f2707e6782e68d49a21896a473c2378ebfa0bc30
Denis Barko <denisbarko@hotmail.com>
2025-09-07 19:19:05 +0300

    removed useless comments

--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -14,23 +14,17 @@ import lombok.Setter;
 @Setter
 public class TeacherInsertDTO {
 
-//    @NotNull(message = "Το όνομα δεν μπορεί να είναι null.")
-//    @Size(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
     @NotNull
     @Size(min = 2)
     private String firstname;
 
-//    @NotNull(message = "Το επώνυμο δεν μπορεί να είναι null.")
-//    @Size(min = 2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
     @NotNull
     @Size(min = 2)
     private String lastname;
 
-//    @Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν μπορεί να είναι μικρότερο από εννιά ψηφία.")
     @Pattern(regexp = "\\d{9,}")
     private String vat;
 
-//    @NotNull(message = "Η περιοχή του καθηγητή δεν μπορεί να είναι κενή.")
     @NotNull
     private Long regionId;
 }
