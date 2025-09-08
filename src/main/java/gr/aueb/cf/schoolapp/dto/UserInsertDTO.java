8081e646cbf288f7922cab7d40c7ca668df71108
Denis Barko <denisbarko@hotmail.com>
2025-09-08 22:24:12 +0300

    fix: add proper message identifiers

--- src/main/java/gr/aueb/cf/schoolapp/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/UserInsertDTO.java
@@ -14,12 +14,14 @@ import lombok.Setter;
 @Setter
 public class UserInsertDTO {
 
-    @NotNull(message = "{NotNull.userInsertDTO.username}")
-    @Size(min = 2, max = 20, message = "{Size.userInsertDTO.username}")
+    @NotNull(message = "{user.username.notnull}")
+    @Size(min = 2, max = 20, message = "{user.username.size}")
     private String username;
 
-    @NotNull(message = "{NotNull.userInsertDTO.username}")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$", message = "{Size.userInsertDTO.username}")
+    @NotNull(message = "{user.password.notnull}")
+    @Size(min=8,  message = "{user.password.size}")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$",
+            message = "{user.password.pattern}")
     private String password;
 
     @NotNull
